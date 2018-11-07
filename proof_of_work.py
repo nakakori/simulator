31e0a2128e7344a85929c470cb62c63f734eba6e
nakakori-mac <nakakori@gmail.com>
2018-11-07 12:00:44 +0900

    Proof-of-Work Simulation

--- proof_of_work.py
+++ proof_of_work.py
@@ -1,36 +1,45 @@
 # coding: utf-8
 
 import numpy as np
-from scipy.stats import bernoulli
-import csv
+from scipy.stats import poisson
+# import csv
 
 # Simulation of Proof-of-Work
-# Difficulty is length of first 0 bit string
-# Bitcoin genesis block's difficulty is 32
 
-max_bit = 256
+# simulation mining function
+def mining(hashrate, difficulty=1):
+    # ポアソン分布のパラメータ
+    p = 1/(difficulty * 2**32)
+    mu = p*hashrate
 
-def mining(hashrate, difficulty=32):
-    # Calculate a probability of generating block from hashrate and difficulty 
-    p = hashrate / 2**difficulty
+    # 1秒毎にポアソン分布に従ってトスをする
+    # 1ならマイニング成功、0なら失敗
+    toss = poisson.rvs(mu, size=3600)
+    mining_time = np.where(toss == 1)[0]
 
-    # 二項分布によって条件を満たすハッシュ値を見つけることができるかどうかをシミュレーション
-    # 1秒毎にブロック生成が成功したかどうかを計算
-    # 3600秒=1時間分の計算をする
-    # 一番小さい値をブロック生成が成功した時間とする
-    success = bernoulli.rvs(p, size=3600)
-    success_index = np.where(success == 1)[0]
-
-    if len(success_index) == 0:
+    if len(mining_time) == 0:
         result = -1
     else: 
-        result = success_index[0]
+        result = mining_time[0]
     
     return result
 
-def main():
-    miners = [10000, 100000, 300000, 500000, 1000000, 5000000]
-    
+# simulation adjust difficulty every 2016 blocks (two weeks)
+def adjust_difficulty(current_diff, blockchain):
+    latest_blocks = blockchain[len(blockchain)-2016:]
+    next_diff = current_diff * (2016 * 600 / sum(latest_blocks))
+    if next_diff < 1:
+        next_diff = 1
+    return next_diff
+
+def success_mining(times):
+    ts = sorted(times)
+    for t in ts:
+        if t != -1:
+            return t
+    return None
+
+def test1_pow(miners):
     for miner in miners:
         results = []
         fail = 0
@@ -45,6 +54,41 @@ def main():
             ave = sum(results)/len(results)
             print("Average time: ", int(ave/60), "m", int(ave%60), "s", sep='')
         print("success: ", len(results), ", fail: ", fail, sep='')
+
+def test2_pow(miners):
+    bc = []
+    diff = 1
+    miner_results = [0] * len(miners)
+
+    c = 1
+    while c <= 2016:
+        result = 0
+        times = []
+        fail = 0
+        for miner in miners:
+            times.append(mining(miner,diff))
+        result = success_mining(times)
+        if result == None:
+            fail += 1
+            continue
+        else:
+            miner_results[times.index(result)] += 1
+            result += 3600*fail
+            # print(c, "回目 : ", int(result/60), "m", int(result%60), "s", sep='')
+            c += 1
+            bc.append(result)
+
+    print("Average time: ", int(sum(bc)/len(bc)/60), "m", int(sum(bc)/len(bc)%60), "s", sep='')
+    print(miner_results)
+    print(adjust_difficulty(diff,bc))
+
+
+def main():
+    # miners = [10000, 100000, 300000, 500000, 1000000, 5000000]
+    miners = [100000] * 70
+    # test1_pow(miners)
+    test2_pow(miners)
+
 
 if __name__ == '__main__':
     main()

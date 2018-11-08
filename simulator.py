7e3aa759fd57f00aca03e57572e9d27216586c73
nakahara14 <nakahara14@sec.kit.ac.jp>
2018-11-08 17:05:28 +0900

    simulator model

--- simulator.py
+++ simulator.py
@@ -1,4 +1,6 @@
 import csv
+import time
+
 import numpy as np
 
 import blockchain
@@ -11,9 +13,13 @@ class Simulator():
 
     def run(self):
         self.init_simulator(self)
-        self.phase1()
-        self.phase2()
-        self.phase3()
+
+        for _ in range(self.config["episode"]):
+            self.phase1()
+            self.phase2()
+            self.phase3()
+
+        self.end_simulator(self)
 
     # Initialization of simulation
     @staticmethod
@@ -21,7 +27,7 @@ class Simulator():
         # Initialization of blockchain
         self.blockchain = blockchain.Blockchain()
         # Initialization of miner
-        for i in range(self.config.num):
+        for i in range(self.config["num"]):
             hr = -1
             while hr < 0:
                 #TODO この正規分布のパラメータもコンフィグで設定できるようにする
@@ -29,6 +35,10 @@ class Simulator():
             self.miners.append(blockchain.Miner(
                 id=i, hashrate=hr, blockchain=self.blockchain))
 
+        # Initialization of data files
+        timestamp = str(int(time.time()))
+        self.log_filepath = "log/" + self.config.protocol + "_" + timestamp + ".log"
+        self.data_filepath = "data/" + self.config.protocol + "_" + timestamp + ".csv"
 
     @staticmethod
     def phase1():
@@ -41,3 +51,8 @@ class Simulator():
     @staticmethod
     def phase3():
         pass
+
+    @staticmethod
+    def end_simulator(self):
+        self.blockchain.write_datafile(self.data_filepath)
+        
